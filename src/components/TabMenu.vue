6b89f108dac82e075a44788c04c75c39200ec78e
key4_0121 <knty130.1704@gmail.com>
2021-02-08 14:53:36 +0900

    header completed

--- src/components/TabMenu.vue
+++ src/components/TabMenu.vue
@@ -1,5 +1,23 @@
 <template>
-  
+
+        <nav class="navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <!-- pc -->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mx-auto">
+
+                      <li class="nav-item "v-for="(item, index) in items" :key=index>
+                         <router-link class="nav-link px-4 py-3" v-bind:to=item.path>
+                            {{ item.title }}
+                         </router-link>
+
+                        </li>
+                    </ul>
+                </div>
+              </div>
+            </nav>
+
+<!--
   <header>
     <ol>
       <li v-for="(item, index) in items" :key=index>
@@ -9,7 +27,7 @@
       </li>
     </ol>
   </header>
-
+-->
 </template>
 
 <script>
@@ -31,73 +49,37 @@ export default {
 </script>
 
 <style scoped>
-html, body,
-ul, ol, li,
-h1, h2, h3, h4, h5, h6, p, div {
-  margin: 0;
-  padding: 0;
-}
 
-header{
-  z-index: 10;
-  height: 64px;
-  position: relative;
-  top: 0px;
-  background-color: #09015F;
-  width: 100%;
-  text-align: center;
-  z-index: 2;
-  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.25);
-  padding: 10px 0px 10px;
+nav {
+    height: 56px;
 }
-
-ol {
-  margin: auto;
-  width: 480px;
-  display:flex;
-  justify-content: center;
-  padding: 0;
-  list-style: none;
+.nav-link:hover {
+    background-color: rgba(9,1,95,0.5);
+    transition: 0.5s;
 }
-
-li {
-  width: 100px;
-  height: 65px;
-  margin-left:10px;
-  margin-right:10px;
-  font-size: 18px;
-  position: relative;
+.sp {
+    display: none;
 }
-
-li::after {
-  background-color: #AF0069;
-  content: "";
-  width: 0;
-  height: 3px;
-  left: 0;
-  bottom: 0;
-  transition: width 0.35s ease 0s;
-  position: absolute;
-}
-
-li:hover::after {
-  width: 100%;
-}
-/*  router-linkがaタグとして表示されてたのでスタイルをaタグに書いた*/
-a {
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  color: blue;
-  text-decoration: none;
-  line-height: 50px;
-}
-
-.picture1 {
-  width: 1000px;
-  height: 500px;
-  background-color: blue;
+@media screen and (max-width:800px){
+    .collapse {
+        display: none;
+    }
+    .sp {
+        display: block;
+    }
+    nav {
+    height: 80px;
+    }
+    ul {
+        overflow: hidden;
+    }
+    ul li {
+        float: left;
+        list-style: none;
+    }
+    .ancar {
+        display: block;
+        padding: 4px 4px;
+    }
 }
 </style>
